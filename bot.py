f85610ccde3b58f139d2ed60b7f105211e82874f
divadop <35879400+divadop@users.noreply.github.com>
2018-01-31 14:45:19 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -361,9 +361,9 @@ async def on_message(mes):
                     pic = random.choice(["http://s2.quickmeme.com/img/8d/8da8c3d8e11da61d7886025a62be2d18f82e1c673007a357471c8a21ae70b8e0.jpg", "https://img.memecdn.com/om-nom-nom_o_2085747.jpg", "https://media.giphy.com/media/YmYemei6DDkrK/giphy.gif", "http://roflzoo.com/pics/052010/bird-om-nom-nom-big.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHUJ14VGR5i5dWaiCLoH1YZx6kP4H7hfHWYgJzp1deYQj_BqGq"])
                     await client.send_message(mes.channel, pic)
             if client.trigger.get(mes.server.id, True):
-                ```if re.search(r" ", mes.content.lower()):
+                 '''if re.search(r" ", mes.content.lower()):
                     ID = mes.author.id
-                    await client.send_message(mes.channel, " <@{0}>.".format(ID))```
+                    await client.send_message(mes.channel, " <@{0}>.".format(ID))'''
                 if re.search(r"porps", mes.content.lower()):
                     ID = mes.author.id
                     await client.send_message(mes.channel, "what do you want from porps, <@{0}> ? he's a busy man ya know".format(ID))
